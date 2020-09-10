2b997a20a1b7ab61b183cb458dbff83c8eab5009
Zach <zacharyjordan82@gmail.com>
2020-09-10 15:53:41 -0400

    adding the node tutorial script

---
+++ node-tutorial-setup.ps1
@@ -0,0 +1,8 @@
+# In order to use these commands you will need Chocolatey installed
+# You can find how to install on their website here: https://chocolatey.org/install
+#
+# If you find that you already have any of these dependencies installed you can just delete the line or comment them out.
+
+cinst nodejs -y
+cinst vscode -y
+cinst git -y
